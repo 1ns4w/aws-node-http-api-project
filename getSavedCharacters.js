a1b3e2cfde02d17c0c08e208fb48514fe31add6d
Angel Mejia <amejiadev@gmail.com>
2022-08-28 18:01:26 -0500

    fix: scan

--- getSavedCharacters.js
+++ getSavedCharacters.js
@@ -1,10 +1,38 @@
 "use strict"
 
-module.exports.getSavedCharacters = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "POST request to /saveCharacter",
-        })
+const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+
+module.exports.getSavedCharacters = async () => {
+
+    const REGION = "us-east-1";
+    const ddbClient = new DynamoDBClient({ region: REGION });
+
+    const params = {
+        TableName: "characters-table-dev",
+        Select: "ALL_ATTRIBUTES"
     };
+
+    try {
+        const response = await ddbClient.send(new ScanCommand(params));
+        
+        const items = response.Items.map(character => {
+            return {
+                id: character.characterId.N,
+                nombre: character.name.S,
+                url: character.url.S
+            }
+        })
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(items)
+        };
+    }
+
+    catch (error) {
+        return {
+            statusCode: error.response.status,
+            body: JSON.stringify(error.response.data)
+        };
+    }
 }
